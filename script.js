26f1088da88eec989a66b1c95e6c63bd3bd860e7
pinkuna1 <pinkunalenka1@gmail.com>
2024-02-17 00:25:48 +0530

    hii

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("hello world");
+let mark = [12,23,45,55,55];
+console.log(mark);
+let veggies = [ "potsto" , "alu" , "baigana"];
+console.log(veggies);
+veggies.push("potalo");
+console.log(veggies);
+let company=["bloomberg" , "microsoft" , "uber", "google", "netflix"];
+
+console.log(company);
+company.shift();
+company.splice(2,1,america);
+//function
+
+
+
+
+
+
